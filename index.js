f83c63c7e16b630cbc3dc07837017d6cb8c62304
nchieune <hieuncce180986@fpt.edu.vn>
2024-11-29 13:37:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+const uploadRoute = require("./controller/routeUpload");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+//the route
+app.use("/api/users", uploadRoute);
+
+//port connection
+app.listen(PORT, () => {
+  console.log(`listening at http://localhost:${PORT}`);
+});
+
+//cloudinary account:  https://cloudinary.com/signup
