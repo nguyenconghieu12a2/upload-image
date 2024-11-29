997a7a42c89ad4d8e0bacb278b4782a93b30b646
nchieune <hieuncce180986@fpt.edu.vn>
2024-11-29 18:01:18 +0700

    update: upload to direct folder

--- controller/routeUpload.js
+++ controller/routeUpload.js
@@ -3,22 +3,37 @@ const router = express.Router();
 const cloudinary = require("../utils/coudinary");
 const upload = require("../middleware/multer");
 
-router.post("/image/upload", upload.single("image"), function (req, res) {
-  cloudinary.uploader.upload(req.file.path, function (err, result) {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({
-        success: false,
-        message: "Error",
+router.post("/image/upload", upload.single("file"), function (req, res) {
+  const folder = req.body.folder;
+
+  if (!folder) {
+    return res.status(400).json({
+      success: false,
+      message: "Folder name is required",
+    });
+  }
+
+  cloudinary.uploader.upload(
+    req.file.path,
+    {
+      folder: folder,
+    },
+    function (err, result) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({
+          success: false,
+          message: "Error during upload",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Uploaded!",
+        data: result,
       });
     }
-
-    res.status(200).json({
-      success: true,
-      message: "Uploaded!",
-      data: result,
-    });
-  });
+  );
 });
 
 module.exports = router;
